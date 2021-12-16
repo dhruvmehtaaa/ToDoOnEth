9ffce2132d5bb7734f7765a1980c96ce6d638afb
dhruvmehtaaa <dhruvmehtaalt@gmail.com>
2021-12-16 16:36:32 +0530

    Done

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-const TodoList = artifacts.require("./TodoList.sol");
+var TodoList = artifacts.require("./TodoList.sol");
 
-module.exports = function (deployer) {
-    deployer.deploy(TodoList);
+module.exports = function(deployer) {
+  deployer.deploy(TodoList);
 };
